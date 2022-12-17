b744d9dca943d888aecac58d068ec7b3f909871b
Darumaism <119983007+Darumaism@users.noreply.github.com>
2022-12-17 22:36:06 +0900

    upgrade:画像

--- faceframe/frame.py
+++ faceframe/frame.py
@@ -5,7 +5,7 @@ HAAR_FILE = "haarcascade_frontalface_default.xml"
 cascade = cv.CascadeClassifier(HAAR_FILE)
 
 #画像の読み込み
-img = cv.imread("sample.jpg")
+img = cv.imread("6632749D-F045-4866-89A2-88EDF54477E5.jpeg")
 
 #顔検出
 face = cascade.detectMultiScale(img)
@@ -14,6 +14,8 @@ face = cascade.detectMultiScale(img)
 for x, y, w, h in face:
     cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),1)
 
-cv.imshow("img", img)
-cv.waitKey(0)
-cv.destroyAllWindows()
+#cv.imshow("img", img)
+#cv.waitKey(0)
+#cv.destroyAllWindows()
+
+cv.imwrite('faceframe.jpg', img)
